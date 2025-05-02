74563e2d9fa295c9547d5b43a5032b8aa36c5772
Matt Gleich <git@mattglei.ch>
2025-05-02 17:10:05 -0400

    feat: improved home page notes view
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { DynamicHead, NavLogo } from '@gleich/ui';
+	import { Card, DynamicHead, NavLogo } from '@gleich/ui';
 	import notes from './notes.json';
 </script>
 
@@ -13,13 +13,40 @@
 	<h1>Matt Gleich's Notes</h1>
 </div>
 
-{#each notes as note (note.slug)}
-	<a href={note.slug}>{note.title}</a>
-{/each}
+<div class="notes">
+	{#each notes as note (note.slug)}
+		<Card>
+			<a href={note.slug} class="note">
+				<h4>{note.title}</h4>
+				<div class="note-data">
+					<p>{note.slug.replaceAll('/', ' → ')}</p>
+				</div>
+			</a>
+		</Card>
+	{/each}
+</div>
 
 <style>
 	.header {
 		display: flex;
 		gap: 10px;
+	}
+
+	.notes {
+		margin-top: 30px;
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+	}
+
+	.note {
+		color: inherit;
+		text-decoration: none;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+	}
+
+	.note-data {
+		color: grey;
 	}
 </style>
