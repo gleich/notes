a4a0e75da94021b1baf77a895f0ec118b03d7d35
Matt Gleich <git@mattglei.ch>
2025-05-02 13:29:19 -0400

    feat: use gleich/ui's layout
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,38 +2,11 @@
 	import type { Snippet } from 'svelte';
 	import '@gleich/ui/styles.css';
 	import '../global.css';
-	import { Copyright } from '@gleich/ui';
+	import { Layout } from '@gleich/ui';
 
 	const { children }: { children: Snippet } = $props();
 </script>
 
-<main>
-	<div class="container">
-		{@render children()}
-		<div class="copyright">
-			<Copyright repo="gleich/notes" />
-		</div>
-	</div>
-</main>
-
-<style>
-	main {
-		min-height: 100vh;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.container {
-		display: flex;
-		flex-direction: column;
-		max-width: 1000px;
-		width: 100%;
-		flex: 1;
-		padding: 10px;
-	}
-
-	.copyright {
-		margin-top: 30px;
-	}
-</style>
+<Layout repo="gleich/notes">
+	{@render children()}
+</Layout>
